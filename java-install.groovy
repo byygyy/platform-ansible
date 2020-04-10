2ae737ba3d77ff65252f11502c25a50ea58c1370
robin <lhh_nj@163.com>
2020-04-10 23:16:23 +0800

    modify the defaultValue

--- java-install.groovy
+++ java-install.groovy
@@ -1,7 +1,7 @@
 node("cm-linux-ansible") {
       properties([
           parameters([
-            string(name:'NODENAME', defaultValue:'cm-linux', description:'input the node name'),
+            string(name:'NODENAME', defaultValue:'cm-linux-ansible', description:'input the node name'),
             string(name:'TARGET', defaultValue:'127.0.0.1', description:'input the server fqdn')
       ])
     ])
